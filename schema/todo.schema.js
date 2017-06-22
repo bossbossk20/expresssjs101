b7353266b3637eb0809ed08453feb67c5cdc8455
thanawat <tha_chookes@hotmail.com>
2017-06-22 18:10:11 +0700

    add files :octocat:

---
+++ schema/todo.schema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const Todo = new mongoose.Schema({
+    name: String,
+    date: {
+       type: Date,
+       default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Todo', Todo)
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost/todolist101')
+
+const Todo = require('./schema/todo.schema')
+
+
+app.get('/showTodoLists', (req ,res) => {
+    Todo.find({}).then((todos) => {
+        res.send(todos)
+    })
+})
+
+app.post('/addTodo', (req, res) => {
+    console.log(req.body)
+    let obj = new Todo(req.body)
+    obj.save((err, obj) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(obj)
+        }
+    })
+})
+
+app.listen(3000)
+
+
